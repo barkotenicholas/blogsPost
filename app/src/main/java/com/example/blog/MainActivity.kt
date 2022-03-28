dcc8b85a0e8f2ab0eff1ef3fd40c2ff3d1c0e523
barkotenicholas <barkotenicholas@gmail.com>
2022-03-28 16:35:20 +0300

    Added internet permission in manifest

--- app/src/main/java/com/example/blog/MainActivity.kt
+++ app/src/main/java/com/example/blog/MainActivity.kt
@@ -2,17 +2,23 @@ package com.example.blog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.blog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
+        binding.button.setOnClickListener {
+            viewModel.getPosts()
+        }
     }
 }
